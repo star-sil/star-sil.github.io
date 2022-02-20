c4c87b3f642dea1f14547e26889f39b07bc9c699
star-sil <kimse9811@naver.com>
2022-02-20 18:21:00 +0900

    update

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"[BOJ] 알파벳(1987)",'url':"/2022/02/20/BOJ1987_%EC%95%8C%ED%8C%8C%EB%B2%B3.html"}]};
